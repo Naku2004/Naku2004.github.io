331b1dcd571fe7ce057cbe37868587eafaf599db
Naku <132116841+Naku2004@users.noreply.github.com>
2023-12-06 12:09:54 -0300

    Updated buttons

--- main.js
+++ main.js
@@ -2,31 +2,3 @@ function cambia_de_pagina(){
     setTimeout(() => { 
         location.href="rutins.html" }, 900);
 }
- 
-var button = 0
-var butonlist = document.getElementById('list');
-var butonrutinas = document.getElementById('rutinas');
-
-function buttonLista(){
-    if (button != 1){
-        button = 1
-        var butonlist = document.getElementById('list');
-        butonlist.classList.add('cambiado')
-        var butonrutinas = document.getElementById('rutinas');
-        butonrutinas.classList.remove('cambiado')
-    }
-}
-
-function buttonRutinas(){
-    if (button != 2){
-        button = 2
-        var butonrutinas = document.getElementById('rutinas');
-        butonrutinas.classList.add('cambiado')
-        var butonlist = document.getElementById('list');
-        butonlist.classList.remove('cambiado')
-    }
-}
-
-butonlist.addEventListener('click', lista)
-butonrutinas.addEventListener('click', buttonRutinas)
-
