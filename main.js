f9146a6c1fc30501e95f10cd123a94572d657b1b
Naku <nahugalindez3105@gmail.com>
2023-12-06 12:04:30 -0300

    Two buttons interaction

--- main.js
+++ main.js
@@ -3,10 +3,30 @@ function cambia_de_pagina(){
         location.href="rutins.html" }, 900);
 }
 
+var button = 0
 var butonlist = document.getElementById('list');
+var butonrutinas = document.getElementById('rutinas');
 
-function lista(){
-    alert('Presionaste el botón');
+function buttonLista(){
+    if (button != 1){
+        button = 1
+        var butonlist = document.getElementById('list');
+        butonlist.classList.add('cambiado')
+        var butonrutinas = document.getElementById('rutinas');
+        butonrutinas.classList.remove('cambiado')
+    }
+}
+
+function buttonRutinas(){
+    if (button != 2){
+        button = 2
+        var butonrutinas = document.getElementById('rutinas');
+        butonrutinas.classList.add('cambiado')
+        var butonlist = document.getElementById('list');
+        butonlist.classList.remove('cambiado')
+    }
 }
 
 butonlist.addEventListener('click', lista)
+butonrutinas.addEventListener('click', buttonRutinas)
+
