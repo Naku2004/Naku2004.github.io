e01041c08c59b56191ef094b91b853c1040d6f6c
Naku <nahugalindez3105@gmail.com>
2023-12-06 01:35:29 -0300

    Button List interaction added

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ function cambia_de_pagina(){
     setTimeout(() => { 
         location.href="rutins.html" }, 900);
 }
+
+var butonlist = document.getElementById('list');
+
+function lista(){
+    alert('Presionaste el botón');
+}
+
+butonlist.addEventListener('click', lista)
